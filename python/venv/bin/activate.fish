21ab619e82b977c2fe96dbbad0f7514cef4cc13e
Jeff Zhao <jeffrey@scurid.com>
2024-06-21 10:29:44 +0900

    Updated venv to include both grpcio and protobuf packages

--- python/venv/bin/activate.fish
+++ python/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jeffreyzhao/Downloads/scuridEdgePythonDemo/venv"
+set -gx VIRTUAL_ENV "/Users/jeffreyzhao/scurid/sample-apps/python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
